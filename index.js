603b15b5030ade33b591de7013bbd458850b0f4d
IPad4610sabu-2 <aihfhauu@gmail.com>
2024-12-17 20:07:48 +0900

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get('/w/:id/5.pdf', async (req, res) => {
         const videoData = response.data;
         console.log(videoData);
 
-        res.render('video', { videoData });
+        res.render('infowatch', { videoData });
   } catch (error) {
         res.status(500).render('matte', { 
       videoId, 
